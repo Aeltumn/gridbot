67d366211ef99b5cf5ea92b0ec25c29c99a65009
Daniël Goossens <danielgoossens@outlook.com>
2018-11-22 12:23:10 +0100

    Trying to catch uncaught exception

--- src/main/cpp/incoming/Logger.cpp
+++ src/main/cpp/incoming/Logger.cpp
@@ -15,51 +15,51 @@
 int sockfd; // socket file descriptor 
 
 void Logger::setup() {
-	int portno = 4447; // port number
-	struct sockaddr_in serv_addr;
-	struct hostent *server;
-
-	info("Starting logger setup...");
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); // generate file descriptor 
-	if (sockfd < 0) {
-		error("Error with setting up socket file descriptor.");
-		return;
-	}
-
-	server = gethostbyname("206.189.111.28"); //the ip address (or server name) of the listening server.
-	if (server == NULL) {
-		error("Error with getting hostname for listening server.");
-		return;
-	}
-
-	bzero((char *)&serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-	serv_addr.sin_port = htons(portno);
-	info("Initialised socket connection target and descriptor, attempting to connect...");
-
 	try {
+		int portno = 4447; // port number
+		struct sockaddr_in serv_addr;
+		struct hostent *server;
+
+		info("Starting logger setup...");
+
+		sockfd = socket(AF_INET, SOCK_STREAM, 0); // generate file descriptor 
+		if (sockfd < 0) {
+			error("Error with setting up socket file descriptor.");
+			return;
+		}
+
+		server = gethostbyname("206.189.111.28"); //the ip address (or server name) of the listening server.
+		if (server == NULL) {
+			error("Error with getting hostname for listening server.");
+			return;
+		}
+
+		bzero((char *)&serv_addr, sizeof(serv_addr));
+		serv_addr.sin_family = AF_INET;
+		bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+		serv_addr.sin_port = htons(portno);
+		info("Initialised socket connection target and descriptor, attempting to connect...");
+
 		if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
 			error("Error with connecting to socket.");
 			return;
 		}
+
+		char rbuff[256];
+		int rbytes;
+
+		info("Succesfully started and connected logger system.");
+		//rbytes = read(sockfd, rbuff, sizeof(rbuff)); // read from socket and store the msg into buffer
+		while (true) {
+			strcat(rbuff, "Received: ");
+			//recv should block until something is received
+			rbytes = recv(sockfd, rbuff, sizeof(rbuff), 0); // similar to read(), but return -1 if socket closed
+			rbuff[rbytes] = '\0'; // set null terminal
+			info(rbuff);
+	}
 	} catch (const std::exception& e) {
 		Logger::info(e.what());
 		return;
-	}
-
-	char rbuff[256];
-	int rbytes;
-
-	info("Succesfully started and connected logger system.");
-	//rbytes = read(sockfd, rbuff, sizeof(rbuff)); // read from socket and store the msg into buffer
-	while (true) {
-		strcat(rbuff, "Received: ");
-		//recv should block until something is received
-		rbytes = recv(sockfd, rbuff, sizeof(rbuff), 0); // similar to read(), but return -1 if socket closed
-		rbuff[rbytes] = '\0'; // set null terminal
-		info(rbuff);
 	}
 }
 
