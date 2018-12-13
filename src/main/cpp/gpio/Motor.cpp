24aa9b1ab8605cc84959f1a737d633bafd63ac1d
Daniël Goossens <danielgoossens@outlook.com>
2018-12-13 13:21:36 +0100

    Changed stepper motor order

--- src/main/cpp/gpio/Motor.cpp
+++ src/main/cpp/gpio/Motor.cpp
@@ -10,42 +10,37 @@ void Motor::moveint(const long double &centimeters) {
 	if(mimic) mimic->moveint(centimeters); //null pointers become FALSE automatically
 	for (long long ind = (long long) std::round(centimeters); ind > 0; --ind) {
 		using namespace std::literals::chrono_literals;
-		///Steps 1 and 3 are the same for (counter)clockwise.
-		///Steps 2 and 4 are reversed, so step 2 for clockwise is step 4 for counterclockwise and visa versa.
-		//Step 1
-		GPIO::set(p1, true); GPIO::set(p2, false); GPIO::set(p3, false); GPIO::set(p4, true);
-		std::this_thread::sleep_for(200ms);
-		
-		//IOOI
-		//IIOO
-		//OIIO
-		//OOII
+		//1 0 0 0
+		GPIO::set(p1, true); GPIO::set(p2, false); GPIO::set(p3, false); GPIO::set(p4, false);
+		std::this_thread::sleep_for(100ms);
 
-		//Step 2
-		if (centimeters > 0) {
-			//Positive = Clockwise
-			GPIO::set(p1, true); GPIO::set(p2, true); GPIO::set(p3, false); GPIO::set(p4, false);
-			std::this_thread::sleep_for(200ms);
-		} else {
-			//Negative = Counter-Clockwise
-			GPIO::set(p1, false); GPIO::set(p2, false); GPIO::set(p3, true); GPIO::set(p4, true);
-			std::this_thread::sleep_for(200ms);
-		}
+		//1 1 0 0
+		GPIO::set(p1, true); GPIO::set(p2, true); GPIO::set(p3, false); GPIO::set(p4, false);
+		std::this_thread::sleep_for(100ms);
 
-		//Step 3
+		//0 1 0 0
+		GPIO::set(p1, false); GPIO::set(p2, true); GPIO::set(p3, false); GPIO::set(p4, false);
+		std::this_thread::sleep_for(100ms);
+
+		//0 1 1 0
 		GPIO::set(p1, false); GPIO::set(p2, true); GPIO::set(p3, true); GPIO::set(p4, false);
-		std::this_thread::sleep_for(200ms);
+		std::this_thread::sleep_for(100ms);
 
-		//Step 4
-		if (centimeters <= 0) {
-			//Negative = Counter-Clockwise
-			GPIO::set(p1, true); GPIO::set(p2, true); GPIO::set(p3, false); GPIO::set(p4, false);
-			std::this_thread::sleep_for(200ms);
-		} else {
-			//Positive = Clockwise
-			GPIO::set(p1, false); GPIO::set(p2, false); GPIO::set(p3, true); GPIO::set(p4, true);
-			std::this_thread::sleep_for(200ms);
-		}
+		//0 0 1 0
+		GPIO::set(p1, false); GPIO::set(p2, false); GPIO::set(p3, true); GPIO::set(p4, false);
+		std::this_thread::sleep_for(100ms);
+
+		//0 0 1 1
+		GPIO::set(p1, false); GPIO::set(p2, false); GPIO::set(p3, true); GPIO::set(p4, true);
+		std::this_thread::sleep_for(100ms);
+
+		//0 0 0 1
+		GPIO::set(p1, false); GPIO::set(p2, false); GPIO::set(p3, false); GPIO::set(p4, true);
+		std::this_thread::sleep_for(100ms);
+
+		//1 0 0 1
+		GPIO::set(p1, true); GPIO::set(p2, false); GPIO::set(p3, false); GPIO::set(p4, true);
+		std::this_thread::sleep_for(100ms);
 	}
 
 	//Cleaup after ourselves
