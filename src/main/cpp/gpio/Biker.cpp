6c2682bbb845194e239f8b7e267c6f4efeb9849f
Daniël Goossens <danielgoossens@outlook.com>
2018-12-13 12:51:07 +0100

    Goddarn non-thread safe queues

--- src/main/cpp/gpio/Biker.cpp
+++ src/main/cpp/gpio/Biker.cpp
@@ -6,12 +6,17 @@ void Biker::setup() {
 	Logger::info("[BIKER] Starting Biker thread.");
 	while (true) { //I'm too lazy to make this secure, safe, efficient or anything, don't judge.
 		if (!actions.empty()) {
-			Logger::info("[BIKER] Executing action..");
-			QueuedAction qa = actions.front();
-			qa.m->moveint(qa.centimeters);
-			actions.pop();
+			//Logger::info("[BIKER] Executing action..");
+			//QueuedAction qa = actions.front();
+			//qa.m->moveint(qa.centimeters);
+			//actions.pop();
 		}
 	}
 }
 
-void Biker::push(QueuedAction qa) { actions.push(qa); }
+void Biker::push(QueuedAction qa) {
+	Logger::info("[BIKER] Executing action..");
+	QueuedAction qa = actions.front();
+	qa.m->moveint(qa.centimeters);
+	actions.pop();
+}
