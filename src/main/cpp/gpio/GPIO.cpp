5e29ba5d4cdb3fb068571ad42713645b09f3f13a
Daniël Goossens <danielgoossens@outlook.com>
2018-12-13 12:25:09 +0100

    Linux compatibility, yay.

--- src/main/cpp/gpio/GPIO.cpp
+++ src/main/cpp/gpio/GPIO.cpp
@@ -22,11 +22,11 @@ void GPIO::set(const int &pin, const bool &value) {
 	if (pin < 0 || pin>26) throw "Pin must be between 0 and 26! (inclusive)";
 	char buf[256];
 	buf[0] = 0;
-	strcat_s(buf, "[GPIO] Setting ");
-	strcat_s(buf, std::to_string(pin).c_str());
-	strcat_s(buf, " to ");
-	strcat_s(buf, std::to_string(value).c_str());
-	strcat_s(buf, ".");
+	strcat(buf, "[GPIO] Setting ");
+	strcat(buf, std::to_string(pin).c_str());
+	strcat(buf, " to ");
+	strcat(buf, std::to_string(value).c_str());
+	strcat(buf, ".");
 	Logger::info(buf);
 	GPIO::setval(pin, value);
 	//if(!value) GPIO::setexport(pin, false); //If we set the output to off, also unexport.
