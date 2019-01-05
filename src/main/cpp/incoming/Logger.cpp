e2ef4fe5a875f35bbd63820eb83d45e2150a1fe9
Daniël Goossens <danielgoossens@outlook.com>
2019-01-05 15:37:20 +0100

    Investigating segmentation faults in the Captain

--- src/main/cpp/incoming/Logger.cpp
+++ src/main/cpp/incoming/Logger.cpp
@@ -65,8 +65,8 @@ void Logger::setup() {
 			//recv should block until something is received
 			rbytes = recv(sockfd, rbuff, sizeof(rbuff), 0); // similar to read(), but return -1 if socket closed
 			rbuff[rbytes] = 0;
-			Captain::handleCommand(rbuff);
 			info(rbuff);
+			//Captain::handleCommand(rbuff);
 		}
 	} catch (const std::exception& e) {
 		info(e.what());
