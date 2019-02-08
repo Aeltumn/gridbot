175a7ddbd9166e6a6582e75ca90d002fe43050ca
Daniël Goossens <danielgoossens@outlook.com>
2019-02-08 12:15:38 +0100

    Testing out when removing some lines

--- src/main/cpp/TicTacToe.cpp
+++ src/main/cpp/TicTacToe.cpp
@@ -31,9 +31,9 @@ void TicTacToe::calculate(Board *board) {
 int TicTacToe::calculateBestMove(Board *board, int depth, bool ai) {
 	std::vector<Entry> cdf;
 	//Firstly if the state of the game is tied or a win were at the end of our tree and we return back up.
-	if (isTie(board)) return 0;
-	else if (isGameOver(board)) return -1;
-	else {
+	//if (isTie(board)) return 0;
+	//else if (isGameOver(board)) return -1;
+	//else {
 		for (int s = 0; s < board->getMaxIndex(); s++) {
 			if (board->atIndex(s) != Figure::EMPTY) continue;
 			board->set(s, Figure::PIECE);
@@ -45,7 +45,7 @@ int TicTacToe::calculateBestMove(Board *board, int depth, bool ai) {
 			if (cdf[i].value > max.value) max = cdf[i];
 		if (depth == 0) return max.key;
 		else return max.value;
-	}
+	//}
 }
 
 bool TicTacToe::isTie(Board *board) {
@@ -71,8 +71,7 @@ bool TicTacToe::isGameOver(Board *board) {
 	//Diagonal
 	testLine(board, &ret, 1, 5, 9);
 	testLine(board, &ret, 3, 5, 7);
-	if (ret == Figure::PIECE) return true;
-	return false;
+	return (ret == Figure::PIECE);
 }
 
 void TicTacToe::testLine(Board *board, Figure *ret, int i, int j, int k) {
